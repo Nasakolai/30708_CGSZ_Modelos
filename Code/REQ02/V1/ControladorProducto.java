45114efe771375df6107951d9b060526d9c89f37
kevanthoc249 <kachalan@espe.edu.ec>
2026-05-10 09:58:09 -0500

    Clean code v0.1

--- Code/REQ02/V1/ControladorProducto.java
+++ Code/REQ02/V1/ControladorProducto.java
@@ -24,7 +24,6 @@ public class ControladorProducto implements ActionListener{
         this.objVista = vista;
         this.objDAO = dao;
 
-        // Conectar botones con acciones
         objVista.btnRegistrar.addActionListener(this);
         objVista.CmbProveedor.addActionListener(this);
         objVista.CmbTipo.addActionListener(this);
@@ -51,7 +50,7 @@ public class ControladorProducto implements ActionListener{
         try {
             double precioKg = Double.parseDouble(precioUnit);
             Producto producto = new Producto(nombre, tipo, precioKg, proveedor);
-            //esto se añadio para que se mande al hacer click en el boton
+            
             objDAO.añadirProducto(producto);
             JOptionPane.showMessageDialog(objVista, "Se añadió "+producto.getNombre());
             
@@ -62,59 +61,7 @@ public class ControladorProducto implements ActionListener{
    
     }
 
-//    private void eliminarEspecie() {
-//        int fila = objVista.tablaProductos.getSelectedRow();
-//        if (fila != -1) {
-//            String nombre = objVista.tablaProductos.getValueAt(fila, 0).toString();
-//            objDAO.eliminarProducto(nombre);
-//            JOptionPane.showMessageDialog(objVista, "Especie eliminada.");
-//            llenarTabla(objVista.tablaProductos);
-//        } else {
-//            JOptionPane.showMessageDialog(objVista, "Seleccione un registro para eliminar.");
-//        }
-//    }
 
-//    private void modificarEspecie() {
-//        int fila = objVista.tablaProductos.getSelectedRow();
-//        if (fila != -1) {
-//            String nombreViejo = objVista.tablaProductos.getValueAt(fila, 0).toString();
-//            String nombreNuevo = objVista.txtNombre.getText().trim();
-//            String tipo = objVista.CmbTipo.getSelectedItem().toString();
-//            String precioUnit = objVista.txtPrecio.getText().trim().replace(',', '.');
-//            String proveedor = objVista.CmbProveedor.getSelectedItem().toString();
-//            try {
-//                double precioUnitario = Double.parseDouble(precioUnit);
-//                Producto nuevaEspecie = new Producto(nombreNuevo, tipo, precioUnitario, proveedor);
-//                objDAO.modificarProducto(nombreViejo, nuevaEspecie);
-//                JOptionPane.showMessageDialog(objVista, "Especie modificada.");
-//                llenarTabla(objVista.tablaProductos);
-//            } catch (NumberFormatException ex) {
-//                JOptionPane.showMessageDialog(objVista, "Formato inválido para precio unitario.");
-//            }
-//        } else {
-//            JOptionPane.showMessageDialog(objVista, "Seleccione un registro para modificar.");
-//        }
-//    }
-//
-//    private void migrarProductos() {
-//        int contador = 0;
-//        for (Producto e : objDAO.migrarLista()) {
-//            if (!objDAO.productoExiste(e.getNombre())) {
-//                objDAO.añadirProducto(e);
-//                contador++;
-//            } else {
-//                JOptionPane.showMessageDialog(objVista, "El Producto " + e.getNombre() + " ya está registrada.");
-//            }
-//        }
-//        JOptionPane.showMessageDialog(objVista, "Se migraron " + contador + " productos.");
-//        llenarTabla(objVista.tablaProductos);
-//    }
-//
-//    private void buscarEspeciePorNombre() {
-//        String nombreBuscar = objVista.CmbBuscarProductos.getSelectedItem().toString();
-//        ArrayList<Producto> lista = objDAO.buscarEspecie(nombreBuscar);
-//        llenarTablaConLista(objVista.tablaProductos, lista);
-//    }
 
     public void llenarTabla(JTable tabla) {
         ArrayList<Producto> lista = objDAO.listarProductos();
